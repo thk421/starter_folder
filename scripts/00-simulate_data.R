fdef29b2ebad6881dd1df67f34236fdcb02cced3
thk.421 <tinak.kim@mail.utoronto.ca>
2024-09-19 14:43:42 -0400

    Updated simulation data file with preamble, workspace setup, and data simulation code.

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,47 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Simulate data for property crime reports in Toronto over the past
+# 10 years, and make a graph showing whether or not the report was cleared.
+# Author: Tina Kim
+# Date: 19 September 2024
+# Contact: tinak.kim@mail.utoronto.ca
+# License: University of Toronto
+# Pre-requisites: Know where to get Toronto property crime data.
 
 
 #### Workspace setup ####
+
+#install.packages("tidyverse")
+#install.packages("janitor")
+#install.packages("knitr")
+#install.packages("opendatatoronto")
+
 library(tidyverse)
-# [...UPDATE THIS...]
+library(janitor)
+library(knitr)
+library(opendatatoronto)
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+
+set.seed(123)
+
+# define parameters
+
+# Use 2014 through 2024 to represent the past 10 years
+year <- 2014:2024
+# Represent each of the divisions in Toronto
+division <- c("D11", "D12", "D13", "D14", "D22", "D23", "D31", "D32", 
+              "D33", "D41", "D42", "D43", "D51", "D52", "D53", "D55")
+# Represent property crime types
+crime <- c("Break and Enter", "Theft", "Fraud")
 
 
+simulated_crime_data <-
+  tibble(
+    # Randomly simulate 500 crime reports in random divisions in random years
+    report_year = sample(year, size = 500, replace = TRUE),
+    division = sample(division, size = 500, replace = TRUE),
+    crime_type = sample(crime, size = 500, replace = TRUE),
+    cleared = sample(c(0, 1), size = 500, replace = TRUE, prob = c(0.5, 0.5))
+  )
 
+head(simulated_crime_data)
